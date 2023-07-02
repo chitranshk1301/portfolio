79fb8cc8ec0e0dade535d2969edaa4afd2407d29
Chitransh Kulshrestha <chitranshkulshrestha1301@gmail.com>
2023-07-03 00:29:57 +0530

    initial setup

---
+++ src/content_option.js
@@ -0,0 +1,106 @@
+const logotext = "Ck";
+const meta = {
+    title: "Chitransh Kulshrestha",
+    description: "A passionate full-stack developer with a love for coffee and an insatiable curiosity for all things tech.",
+};
+
+const introdata = {
+    title: "I’m Chitransh Kulshrestha",
+    animated: {
+        first: "I love programming",
+        second: "I am a MERN stack developer",
+        third: "I am a software engineering student",
+    },
+    description: "A passionate full-stack developer with a love for coffee and an insatiable curiosity for all things tech.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "A bit about my self",
+    aboutme: "Passionate full stack developer with expertise in front-end and back-end technologies. Skilled in HTML, CSS, JavaScript, Python, and C++, and experienced with frameworks like React and Node.js. Committed to delivering scalable and user-centric web applications. Constantly seeking opportunities to expand knowledge and stay up-to-date with emerging technologies. Thrives in collaborative environments, contributing to impactful digital experiences. Let's connect and explore the limitless possibilities of full stack development together!",
+};
+const worktimeline = [{
+        jobtitle: "Full Stack Developer",
+        where: "Patilkaki",
+        date: "Dec-2022 - Jan-2023",
+    },
+    {
+        jobtitle: "Web Developer",
+        where: "GUC IIIT Bhopal",
+        date: "Aug-2022 - Present",
+    },
+];
+
+const skills = [{
+        name: "React",
+        value: 90,
+    },
+    {
+        name: "Express",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 85,
+    },
+    {
+        name: "HTML && CSS",
+        value: 90,
+    },
+    {
+        name: "MongoDB, Postgres",
+        value: 85,
+    },
+    {
+        name: "FastAPI",
+        value: 70,
+    },
+];
+
+const services = [{
+        title: "Full Stack Development",
+        description: "As a Full Stack Developer, I am capable of comprehensive web application development expertise in both front-end and back-end technologies. From creating user-friendly interfaces to implementing secure authentication systems.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "chitranshkulshrestha1301@gmail.com",
+    YOUR_FONE: "+91-6264234676",
+    description: "Feel free to contact me via email. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_d7l16ia",
+    YOUR_TEMPLATE_ID: "template_9rdbnk9",
+    YOUR_USER_ID: "chitranshkulshrestha1301@gmail.com",
+};
+
+const socialprofils = {
+    github: "https://github.com/chitranshk1301",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com/in/chitransh-kulshrestha",
+    twitter: "https://twitter.com/chitranshk13",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
